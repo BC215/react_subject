2031c2859acfe5d1515d1dfcc5299cbb18b42727
BC215 <dlqudckd3@gmail.com>
2026-03-24 09:23:11 +0900

    Update LectureService.java

--- react_subject_back/src/main/java/kr/co/iei/lecture/service/LectureService.java
+++ react_subject_back/src/main/java/kr/co/iei/lecture/service/LectureService.java
@@ -12,7 +12,7 @@ import kr.co.iei.lecture.model.vo.Lecture;
 public class LectureService {
     private final LectureDao lectureDao;
 
-    // 정렬/카테고리/레벨/제목 검색 파라미터를 모두 받는 메서드
+
     public List<Lecture> getAll(String sort, Integer category, Integer level, String title) {
         return lectureDao.selectAll(sort, category, level, title);
     }
